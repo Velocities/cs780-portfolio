ef2ef1269d3237d3d5a2c675fc8f1011baf879b5
Velocities <camy624@comcast.net>
2026-05-03 22:37:26 -0400

    fix: typo on AboutMe page

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -8,7 +8,7 @@ export function AboutMe() {
                 I'm Cameron Green, a software engineer completing a Computer Science major
                 at the <a href="https://www.unh.edu" target='_blank' className='text-blue-400 underline hover:text-blue-200'>University of New Hampshire</a>.
                 Becoming a successful software engineer is a goal I strive to achieve in my career,
-                which requires that I become knowledgable on standards and good practices surrounding
+                which requires that I become knowledgeable on standards and good practices surrounding
                 user experiences and accessibility. On this webpage, I describe these various standards
                 and good practices that I learned from assignments during my time in CS 780 Human-Computer
                 Interaction at UNH.
